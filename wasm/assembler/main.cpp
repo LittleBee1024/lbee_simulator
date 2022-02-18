b7b0a17d9da73a02d78b9f8feb997ba761c767a5
yuxiangw <yuxian660@163.com>
2022-02-18 15:54:50 +0800

    clean test

--- wasm/assembler/main.cpp
+++ wasm/assembler/main.cpp
@@ -17,39 +17,14 @@ static bool endsWith(const std::string& str, const std::string& suffix)
       0 == str.compare(str.size()-suffix.size(), suffix.size(), suffix);
 }
 
-int main_func(int argc, char *argv[])
+int mainImpl(int argc, char *argv[])
 {
    std::string infname;
    std::string outfname;
-   int nextarg = 1;
    if (argc < 2)
       usage(argv[0]);
-   while (argv[nextarg][0] == '-')
-   {
-      char flag = argv[nextarg][1];
-      switch (flag)
-      {
-      case 'V':
-         LBEE_YAS::vcode = 1;
-         if (argv[nextarg][2])
-         {
-            LBEE_YAS::block_factor = atoi(argv[nextarg] + 2);
-            if (LBEE_YAS::block_factor != 8)
-            {
-               fprintf(stderr, "Unknown blocking factor %d\n", LBEE_YAS::block_factor);
-               exit(1);
-            }
-         }
-         nextarg++;
-         break;
-      case 'v':
-         nextarg++;
-         break;
-      default:
-         usage(argv[0]);
-      }
-   }
-   infname = argv[nextarg];
+
+   infname = argv[1];
    if (!endsWith(infname, ".ys"))
       usage(argv[0]);
 
@@ -66,30 +41,21 @@ int main_func(int argc, char *argv[])
       exit(1);
    }
 
-   if (LBEE_YAS::vcode)
+   outfname = infname.substr(0, infname.find_last_of('.')) + ".yo";
+   LBEE_YAS::outfile = fopen(outfname.c_str(), "w");
+   if (!LBEE_YAS::outfile)
    {
-      LBEE_YAS::outfile = stdout;
-   }
-   else
-   {
-      outfname = infname.substr(0, infname.find_last_of('.')) + ".yo";
-      LBEE_YAS::outfile = fopen(outfname.c_str(), "w");
-      if (!LBEE_YAS::outfile)
-      {
-         fprintf(stderr, "Can't open output file '%s'\n", outfname.c_str());
-         exit(1);
-      }
+      fprintf(stderr, "Can't open output file '%s'\n", outfname.c_str());
+      exit(1);
    }
 
    LBEE_YAS::pass = 1;
-
    yaslex();
    fclose(yasin);
 
    if (LBEE_YAS::hit_error)
       exit(1);
 
-   LBEE_YAS::pass = 2;
    yasin = fopen(infname.c_str(), "r");
    if (!yasin)
    {
@@ -97,6 +63,7 @@ int main_func(int argc, char *argv[])
       exit(1);
    }
 
+   LBEE_YAS::pass = 2;
    yaslex();
    fclose(yasin);
    fclose(LBEE_YAS::outfile);
@@ -105,5 +72,5 @@ int main_func(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-   return main_func(argc, argv);
+   return mainImpl(argc, argv);
 }
