43a2945578cdb7e338d96dd66303615ee971e8e4
yuxiangw <yuxian660@163.com>
2022-02-19 18:18:49 +0800

    move func to class

--- wasm/assembler/lexer.cpp
+++ wasm/assembler/lexer.cpp
@@ -1,10 +1,10 @@
 #include "lexer.h"
-#include "isa.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdexcept>
+#include <vector>
 
 /***************************************
  * Variables and functions from flex
@@ -13,33 +13,22 @@ extern FILE *yasin;
 extern int yaslineno;
 extern int yaslex(YasLexer *);
 
-namespace
-{
 #define STRMAX 4096
 #define TOK_PER_LINE 12
 #define STAB 1000
 #define INIT_CNT 0
 
-   // Token types
-   typedef enum
-   {
-      TOK_IDENT,
-      TOK_NUM,
-      TOK_REG,
-      TOK_INSTR,
-      TOK_PUNCT,
-      TOK_ERR
-   } token_t;
+// Token representation
+typedef struct
+{
+   char *sval;   /* String    */
+   word_t ival;  /* Integer   */
+   char cval;    /* Character */
+   token_t type; /* Type    */
+} token_rec, *token_ptr;
 
-   // Token representation
-   typedef struct
-   {
-      char *sval;   /* String    */
-      word_t ival;  /* Integer   */
-      char cval;    /* Character */
-      token_t type; /* Type    */
-   } token_rec, *token_ptr;
-
+namespace
+{
    // Storage of current line
    char input_line[STRMAX];
    // Storage for strings in current line
@@ -80,265 +69,6 @@ namespace
       int pos;
    } symbol_table[STAB];
 
-   void fail(const char *message)
-   {
-      if (!error_mode)
-      {
-         fprintf(stderr, "Error on line %d: %s\n", yaslineno, message);
-         fprintf(stderr, "Line %d, Byte 0x%.4x: %s\n",
-                 yaslineno, bytepos, input_line);
-      }
-      error_mode = 1;
-      hit_error = 1;
-   }
-
-   void start_line()
-   {
-      int t;
-      error_mode = 0;
-      tpos = 0;
-      tcount = 0;
-      bcount = 0;
-      strpos = 0;
-      for (t = 0; t < TOK_PER_LINE; t++)
-         tokens[t].type = TOK_ERR;
-   }
-
-   void add_token(token_t type, char *s, word_t i, char c)
-   {
-      char *t = NULL;
-      if (!tcount)
-         start_line();
-      if (tpos >= TOK_PER_LINE - 1)
-      {
-         fail("Line too long");
-         return;
-      }
-      if (s)
-      {
-         int len = strlen(s) + 1;
-         if (strpos + len > STRMAX)
-         {
-            fail("Line too long");
-            return;
-         }
-         snprintf(strbuf + strpos, len, "%s", s);
-         t = strbuf + strpos;
-         strpos += len;
-      }
-      tokens[tcount].type = type;
-      tokens[tcount].sval = t;
-      tokens[tcount].ival = i;
-      tokens[tcount].cval = c;
-      tcount++;
-   }
-
-   void hexstuff(char *dest, word_t value, int len)
-   {
-      int i;
-      for (i = 0; i < len; i++)
-      {
-         char c;
-         int h = (value >> 4 * i) & 0xF;
-         c = h < 10 ? h + '0' : h - 10 + 'a';
-         dest[len - i - 1] = c;
-      }
-   }
-
-   void add_symbol(char *name, int p)
-   {
-      size_t len = strlen(name) + 1;
-      char *t = reinterpret_cast<char *>(malloc(len));
-      snprintf(t, len, "%s", name);
-      symbol_table[symbol_cnt].name = t;
-      symbol_table[symbol_cnt].pos = p;
-      symbol_cnt++;
-   }
-
-   int find_symbol(char *name)
-   {
-      int i;
-      for (i = 0; i < symbol_cnt; i++)
-         if (strcmp(name, symbol_table[i].name) == 0)
-            return symbol_table[i].pos;
-      fail("Can't find label");
-      return -1;
-   }
-
-   /* Parse Register from set of tokens and put into high or low
-   4 bits of code[codepos] */
-   void get_reg(int codepos, int hi)
-   {
-      int rval = REG_NONE;
-      char c;
-      if (tokens[tpos].type != TOK_REG)
-      {
-         fail("Expecting Register ID");
-         return;
-      }
-      else
-      {
-         rval = find_register(tokens[tpos].sval);
-      }
-      /* Insert into output */
-      c = code[codepos];
-      if (hi)
-         c = (c & 0x0F) | (rval << 4);
-      else
-         c = (c & 0xF0) | rval;
-      code[codepos] = c;
-      tpos++;
-   }
-
-   /* Get memory reference.
-   Can be of form:
-   Num(Reg)
-   (Reg)
-   Num
-   Ident
-   Ident(Reg)
-   Put Reg in low position of current byte, and Number in following bytes
-   */
-   void get_mem(int codepos)
-   {
-      char rval = REG_NONE;
-      word_t val = 0;
-      int i;
-      char c;
-      token_t type = tokens[tpos].type;
-      /* Deal with optional displacement */
-      if (type == TOK_NUM)
-      {
-         val = tokens[tpos++].ival;
-         type = tokens[tpos].type;
-      }
-      else if (type == TOK_IDENT)
-      {
-         val = find_symbol(tokens[tpos++].sval);
-         type = tokens[tpos].type;
-      }
-      /* Check for optional register */
-      if (type == TOK_PUNCT)
-      {
-         if (tokens[tpos].cval == '(')
-         {
-            tpos++;
-            if (tokens[tpos].type == TOK_REG)
-               rval = find_register(tokens[tpos++].sval);
-            else
-            {
-               fail("Expecting Register Id");
-               return;
-            }
-            if (tokens[tpos].type != TOK_PUNCT || tokens[tpos++].cval != ')')
-            {
-               fail("Expecting ')'");
-               return;
-            }
-         }
-      }
-      c = (code[codepos] & 0xF0) | (rval & 0xF);
-      code[codepos++] = c;
-      for (i = 0; i < 8; i++)
-         code[codepos + i] = (val >> (i * 8)) & 0xFF;
-   }
-
-   /* Get numeric value of given number of bytes */
-   /* Offset indicates value to subtract from number (for PC relative) */
-   void get_num(int codepos, int bytes, int offset)
-   {
-      word_t val = 0;
-      int i;
-      if (tokens[tpos].type == TOK_NUM)
-      {
-         val = tokens[tpos].ival;
-      }
-      else if (tokens[tpos].type == TOK_IDENT)
-      {
-         val = find_symbol(tokens[tpos].sval);
-      }
-      else
-      {
-         fail("Number Expected");
-         return;
-      }
-      val -= offset;
-      for (i = 0; i < bytes; i++)
-         code[codepos + i] = (val >> (i * 8)) & 0xFF;
-      tpos++;
-   }
-   /**
-    * Printing format:
-    *  0xHHHH: cccccccccccccccccccc | <line>
-    *      where HHHH is address
-    *      cccccccccccccccccccc is code
-    */
-   void print_code(FILE *out, int pos)
-   {
-      char outstring[33];
-      if (pos > 0xFFF)
-      {
-         if (tcount)
-         {
-            int i;
-            if (pos > 0xFFFF)
-            {
-               fail("Code address limit exceeded");
-               exit(1);
-            }
-            snprintf(outstring, sizeof(outstring), "0x0000:                      | ");
-            hexstuff(outstring + 2, pos, 4);
-            for (i = 0; i < bcount; i++)
-               hexstuff(outstring + 7 + 2 * i, code[i] & 0xFF, 2);
-         }
-         else
-            snprintf(outstring, sizeof(outstring), "                             | ");
-      }
-      else
-      {
-         if (tcount)
-         {
-            int i;
-            if (pos > 0xFFF)
-            {
-               fail("Code address limit exceeded");
-               exit(1);
-            }
-            snprintf(outstring, sizeof(outstring), "0x000:                      | ");
-            hexstuff(outstring + 2, pos, 3);
-            for (i = 0; i < bcount; i++)
-               hexstuff(outstring + 7 + 2 * i, code[i] & 0xFF, 2);
-         }
-         else
-            snprintf(outstring, sizeof(outstring), "                            | ");
-      }
-      if (vcode)
-      {
-         fprintf(out, "//%s%s\n", outstring, input_line);
-         if (tcount)
-         {
-            int i;
-            for (i = 0; tcount && i < bcount; i++)
-            {
-               if (block_factor)
-               {
-                  fprintf(out, "    bank%d[%d] = 8\'h%.2x;\n",
-                          (pos + i) % block_factor,
-                          (pos + i) / block_factor,
-                          code[i] & 0xFF);
-               }
-               else
-               {
-                  fprintf(out, "    mem[%d] = 8\'h%.2x;\n", pos + i, code[i] & 0xFF);
-               }
-            }
-         }
-      }
-      else
-      {
-         fprintf(out, "%s%s\n", outstring, input_line);
-      }
-   }
 }
 
 YasLexer::YasLexer(const char *inFilename) : m_in(nullptr),
@@ -595,4 +325,264 @@ void YasLexer::finish_line()
 
    print_code(outfile, savebytepos);
    start_line();
+}
+
+void YasLexer::fail(const char *message)
+{
+   if (!error_mode)
+   {
+      fprintf(stderr, "Error on line %d: %s\n", yaslineno, message);
+      fprintf(stderr, "Line %d, Byte 0x%.4x: %s\n",
+              yaslineno, bytepos, input_line);
+   }
+   error_mode = 1;
+   hit_error = 1;
+}
+
+void YasLexer::start_line()
+{
+   int t;
+   error_mode = 0;
+   tpos = 0;
+   tcount = 0;
+   bcount = 0;
+   strpos = 0;
+   for (t = 0; t < TOK_PER_LINE; t++)
+      tokens[t].type = TOK_ERR;
+}
+
+void YasLexer::add_token(token_t type, char *s, word_t i, char c)
+{
+   char *t = NULL;
+   if (!tcount)
+      start_line();
+   if (tpos >= TOK_PER_LINE - 1)
+   {
+      fail("Line too long");
+      return;
+   }
+   if (s)
+   {
+      int len = strlen(s) + 1;
+      if (strpos + len > STRMAX)
+      {
+         fail("Line too long");
+         return;
+      }
+      snprintf(strbuf + strpos, len, "%s", s);
+      t = strbuf + strpos;
+      strpos += len;
+   }
+   tokens[tcount].type = type;
+   tokens[tcount].sval = t;
+   tokens[tcount].ival = i;
+   tokens[tcount].cval = c;
+   tcount++;
+}
+
+void YasLexer::hexstuff(char *dest, word_t value, int len)
+{
+   int i;
+   for (i = 0; i < len; i++)
+   {
+      char c;
+      int h = (value >> 4 * i) & 0xF;
+      c = h < 10 ? h + '0' : h - 10 + 'a';
+      dest[len - i - 1] = c;
+   }
+}
+
+void YasLexer::add_symbol(char *name, int p)
+{
+   size_t len = strlen(name) + 1;
+   char *t = reinterpret_cast<char *>(malloc(len));
+   snprintf(t, len, "%s", name);
+   symbol_table[symbol_cnt].name = t;
+   symbol_table[symbol_cnt].pos = p;
+   symbol_cnt++;
+}
+
+int YasLexer::find_symbol(char *name)
+{
+   int i;
+   for (i = 0; i < symbol_cnt; i++)
+      if (strcmp(name, symbol_table[i].name) == 0)
+         return symbol_table[i].pos;
+   fail("Can't find label");
+   return -1;
+}
+
+/* Parse Register from set of tokens and put into high or low
+   4 bits of code[codepos] */
+void YasLexer::get_reg(int codepos, int hi)
+{
+   int rval = REG_NONE;
+   char c;
+   if (tokens[tpos].type != TOK_REG)
+   {
+      fail("Expecting Register ID");
+      return;
+   }
+   else
+   {
+      rval = find_register(tokens[tpos].sval);
+   }
+   /* Insert into output */
+   c = code[codepos];
+   if (hi)
+      c = (c & 0x0F) | (rval << 4);
+   else
+      c = (c & 0xF0) | rval;
+   code[codepos] = c;
+   tpos++;
+}
+
+/* Get memory reference.
+   Can be of form:
+   Num(Reg)
+   (Reg)
+   Num
+   Ident
+   Ident(Reg)
+   Put Reg in low position of current byte, and Number in following bytes
+   */
+void YasLexer::get_mem(int codepos)
+{
+   char rval = REG_NONE;
+   word_t val = 0;
+   int i;
+   char c;
+   token_t type = tokens[tpos].type;
+   /* Deal with optional displacement */
+   if (type == TOK_NUM)
+   {
+      val = tokens[tpos++].ival;
+      type = tokens[tpos].type;
+   }
+   else if (type == TOK_IDENT)
+   {
+      val = find_symbol(tokens[tpos++].sval);
+      type = tokens[tpos].type;
+   }
+   /* Check for optional register */
+   if (type == TOK_PUNCT)
+   {
+      if (tokens[tpos].cval == '(')
+      {
+         tpos++;
+         if (tokens[tpos].type == TOK_REG)
+            rval = find_register(tokens[tpos++].sval);
+         else
+         {
+            fail("Expecting Register Id");
+            return;
+         }
+         if (tokens[tpos].type != TOK_PUNCT || tokens[tpos++].cval != ')')
+         {
+            fail("Expecting ')'");
+            return;
+         }
+      }
+   }
+   c = (code[codepos] & 0xF0) | (rval & 0xF);
+   code[codepos++] = c;
+   for (i = 0; i < 8; i++)
+      code[codepos + i] = (val >> (i * 8)) & 0xFF;
+}
+
+/* Get numeric value of given number of bytes */
+/* Offset indicates value to subtract from number (for PC relative) */
+void YasLexer::get_num(int codepos, int bytes, int offset)
+{
+   word_t val = 0;
+   int i;
+   if (tokens[tpos].type == TOK_NUM)
+   {
+      val = tokens[tpos].ival;
+   }
+   else if (tokens[tpos].type == TOK_IDENT)
+   {
+      val = find_symbol(tokens[tpos].sval);
+   }
+   else
+   {
+      fail("Number Expected");
+      return;
+   }
+   val -= offset;
+   for (i = 0; i < bytes; i++)
+      code[codepos + i] = (val >> (i * 8)) & 0xFF;
+   tpos++;
+}
+/**
+    * Printing format:
+    *  0xHHHH: cccccccccccccccccccc | <line>
+    *      where HHHH is address
+    *      cccccccccccccccccccc is code
+    */
+void YasLexer::print_code(FILE *out, int pos)
+{
+   char outstring[33];
+   if (pos > 0xFFF)
+   {
+      if (tcount)
+      {
+         int i;
+         if (pos > 0xFFFF)
+         {
+            fail("Code address limit exceeded");
+            exit(1);
+         }
+         snprintf(outstring, sizeof(outstring), "0x0000:                      | ");
+         hexstuff(outstring + 2, pos, 4);
+         for (i = 0; i < bcount; i++)
+            hexstuff(outstring + 7 + 2 * i, code[i] & 0xFF, 2);
+      }
+      else
+         snprintf(outstring, sizeof(outstring), "                             | ");
+   }
+   else
+   {
+      if (tcount)
+      {
+         int i;
+         if (pos > 0xFFF)
+         {
+            fail("Code address limit exceeded");
+            exit(1);
+         }
+         snprintf(outstring, sizeof(outstring), "0x000:                      | ");
+         hexstuff(outstring + 2, pos, 3);
+         for (i = 0; i < bcount; i++)
+            hexstuff(outstring + 7 + 2 * i, code[i] & 0xFF, 2);
+      }
+      else
+         snprintf(outstring, sizeof(outstring), "                            | ");
+   }
+   if (vcode)
+   {
+      fprintf(out, "//%s%s\n", outstring, input_line);
+      if (tcount)
+      {
+         int i;
+         for (i = 0; tcount && i < bcount; i++)
+         {
+            if (block_factor)
+            {
+               fprintf(out, "    bank%d[%d] = 8\'h%.2x;\n",
+                       (pos + i) % block_factor,
+                       (pos + i) / block_factor,
+                       code[i] & 0xFF);
+            }
+            else
+            {
+               fprintf(out, "    mem[%d] = 8\'h%.2x;\n", pos + i, code[i] & 0xFF);
+            }
+         }
+      }
+   }
+   else
+   {
+      fprintf(out, "%s%s\n", outstring, input_line);
+   }
 }
--- wasm/assembler/lexer.h
+++ wasm/assembler/lexer.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include "isa.h"
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
+
+// Token types
+typedef enum
+{
+   TOK_IDENT,
+   TOK_NUM,
+   TOK_REG,
+   TOK_INSTR,
+   TOK_PUNCT,
+   TOK_ERR
+} token_t;
 
 class YasLexer
 {
@@ -24,6 +37,18 @@ public:
    void add_ident(char *);
    void error(const char *);
    void finish_line();
+
+private:
+   void fail(const char *message);
+   void start_line();
+   void add_token(token_t type, char *s, word_t i, char c);
+   void hexstuff(char *dest, word_t value, int len);
+   void add_symbol(char *name, int p);
+   int find_symbol(char *name);
+   void get_reg(int codepos, int hi);
+   void get_mem(int codepos);
+   void get_num(int codepos, int bytes, int offset);
+   void print_code(FILE *out, int pos);
 
 private:
    void resetYasIn();
