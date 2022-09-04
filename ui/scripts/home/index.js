e190ffba8263558b5a19dffbdb1f9f1cea8a36b0
Ben <yuxiang660@163.com>
2022-09-04 10:45:53 +0800

    add icon

--- ui/scripts/home/index.js
+++ ui/scripts/home/index.js
@@ -5,7 +5,12 @@ const vHome = {
             <img alt="LittleBee" src="images/logo.png" width="200">
          </h1>
          <h1>Y86-64 Simulator</h1>
-         <p>Powered by Vue3 + Element Plus + Webassembly + CPP</p>
+         <p>
+            Powered by Vue3 + Element Plus + Webassembly + CPP
+            <a href="https://github.com/LittleBee1024/lbee_simulator">
+               <el-icon><Position /></el-icon>
+            </a>
+         </p>
       </div>
    `
 }
