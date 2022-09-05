b90d85923ad4ded21d09ad7e28061c7a2d120c65
yuxiang660 <yuxiang660@users.noreply.github.com>
2022-09-05 03:53:22 +0000

    Automated deployment: Mon Sep  5 03:53:22 UTC 2022 e7a9323afcb5f5230d7e55c8e1a45044cf9ecdf3

--- wasm/global.h
+++ wasm/global.h
@@ -2,9 +2,26 @@
 
 #pragma once
 
+#include "./simulator/sim_yis.h"
+
+#include <memory>
 #include <string>
 
 namespace GLOBAL
 {
+   // global var to store binary code after assembly
+   extern std::string binCode;
+   // global simulator output
+   extern std::shared_ptr<IO::OutputInterface> simOut;
 
+   class SimSingleton
+   {
+   public:
+      static SIM::Yis *getInstance();
+
+   private:
+      SimSingleton() = delete;
+      SimSingleton(const SimSingleton &) = delete;
+      SimSingleton &operator=(const SimSingleton &) = delete;
+   };
 }
