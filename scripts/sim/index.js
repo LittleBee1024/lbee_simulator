961afd3811b3329e4fcb0ed584c9cbbdf756ee8d
yuxiang660 <yuxiang660@users.noreply.github.com>
2022-09-05 05:16:32 +0000

    Automated deployment: Mon Sep  5 05:16:32 UTC 2022 72da4647a65d12f1699ec3c9f49f9ca1d91bcde1

--- scripts/sim/index.js
+++ scripts/sim/index.js
@@ -1,5 +1,6 @@
 import vAsmCode from './asm_code.js'
 import vBinCode from './bin_code.js'
+import vLoadCode from './load_code.js'
 
 const vSim = {
    data() {
@@ -15,11 +16,13 @@ const vSim = {
    components: {
       'vasmcode': vAsmCode,
       'vbincode': vBinCode,
+      'vloadcode': vLoadCode,
    },
    template: `
       <div>
          <vasmcode :bin-code="binCode" @assemble="setBinCode"/>
          <vbincode :bin-code="binCode" />
+         <vloadcode :bin-code="binCode" />
       </div>
    `
 }
---
+++ scripts/sim/load_code.js
@@ -0,0 +1,8 @@
+const vLoadCode = {
+   props: ['binCode'],
+   template: `
+      <el-button type="primary" icon="Upload" @click="load">Load</el-button>
+   `
+}
+
+export default vLoadCode;
