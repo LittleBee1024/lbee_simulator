ff62e0319bedb588f374f6defe94f1733d927dc8
Ben <yuxiang660@163.com>
2022-09-06 11:17:52 +0800

    update ui

---
+++ ui/scripts/sim/run.js
@@ -0,0 +1,38 @@
+const vRun = {
+   data() {
+      return {
+         activeAddr: -1
+      }
+   },
+   emits: ['updateActiveAddr'],
+   methods: {
+      run() {
+         console.log("run")
+      },
+      step() {
+         console.log("step")
+         this.activeAddr++
+         this.$emit("updateActiveAddr", this.activeAddr)
+      },
+      stop() {
+         console.log("stop")
+      },
+      reset() {
+         console.log("reset")
+      }
+   },
+   template: `
+      <el-row>
+         <el-button type="primary" icon="VideoPlay" @click="run">Run</el-button>
+         <el-button type="primary" icon="ArrowRight" @click="step">Step</el-button>
+         <el-button type="warning" icon="VideoPause" @click="stop">Stop</el-button>
+         <el-button type="warning" icon="RefreshLeft" @click="reset">Reset</el-button>
+      </el-row>
+      <el-divider>Y86-64 Registers</el-divider>
+      <el-divider>Y86-64 Data Memory Diff</el-divider>
+   `
+}
+
+export default vRun;
+
+
