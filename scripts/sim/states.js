5d145572e8321041fb19196b68e42ceb23c6390d
yuxiang660 <yuxiang660@users.noreply.github.com>
2022-09-06 16:28:14 +0000

    Automated deployment: Tue Sep  6 16:28:14 UTC 2022 9dac20e8e40c367c7d257dc0a9aabf8724fca150

--- scripts/sim/states.js
+++ scripts/sim/states.js
@@ -1,18 +1,37 @@
-const toUInt64 = function(low32, high32) {
+const toUInt64 = function (low32, high32) {
    return (BigInt(high32) << BigInt(32)) ^ BigInt(low32)
 }
 
 const vStates = {
    props: ['update'],
+   data() {
+      return {
+         regs: [
+            { 'RAX': 0x0 },
+            { 'RCX': 0x0 },
+            { 'RDX': 0x0 },
+            { 'RBX': 0x0 },
+            { 'RSP': 0x0 },
+            { 'RBP': 0x0 },
+            { 'RSI': 0x0 },
+            { 'RDI': 0x0 },
+            { 'R8' : 0x0 },
+            { 'R9' : 0x0 },
+            { 'R10': 0x0 },
+            { 'R11': 0x0 },
+            { 'R12': 0x0 },
+            { 'R13': 0x0 },
+            { 'R14': 0x0 },
+         ]
+      }
+   },
    methods: {
       updateRegisters() {
-         console.log("updateRegisters")
          const regBase = Module._Sim_Get_Registers();
-         const NUM_REGS = 15
-         for (let i = 0; i < NUM_REGS; i++) {
+         for (let i = 0; i < this.regs.length; i++) {
             const addr = regBase + i * BigInt64Array.BYTES_PER_ELEMENT
             const regVal = toUInt64(Module.HEAPU32[addr >> 2], Module.HEAPU32[(addr + 4) >> 2])
-            console.log(regVal.toString(16))
+            Object.keys(this.regs[i]).forEach(key => {this.regs[i][key] = regVal})
          }
       },
       updateDataMemory() {
@@ -27,6 +46,15 @@ const vStates = {
    },
    template: `
       <el-divider>Y86-64 Registers</el-divider>
+         <el-descriptions
+            :column="4"
+            direction="vertical"
+            border
+         >
+            <el-descriptions-item v-for="reg in regs" :label="Object.keys(reg)[0]">
+               {{ '0x' + Object.values(reg)[0].toString(16) }}
+            </<el-descriptions-item>
+         </el-descriptions>
       <el-divider>Y86-64 Data Memory Diff</el-divider>
    `
 }
